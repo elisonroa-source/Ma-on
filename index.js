95e0e744ec5da4ed748c67af76cf8b3534bf25e6
Elison Roa <elisonroa@gmail.com>
2026-09-17 20:48:36 -0400

    feat: agrega pagina inicial con HTML y JavaScript

---
+++ index.js
@@ -0,0 +1,10 @@
+let nombre = prompt("Cual es tu nombre");
+let apellido = prompt("Cual es tu apellido");
+let anoNacimiento = prompt("En que año naciste");
+let edad = 2026 - anoNacimiento;
+let esMayorEdad = edad > 18 ? "Eres mayor de edad" : "Eres menor de edad";
+
+let mensaje = `Hola ${nombre} ${apellido} usted nacio${anoNacimiento} y tienes ${edad}, y usted es${esMayorEdad}`;
+
+alert(mensaje);
+
